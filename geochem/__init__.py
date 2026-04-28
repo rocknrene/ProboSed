d49ba03aee4c68acc435036eb3254b2daad62cf2
Rene Castillo <110206995+rocknrene@users.noreply.github.com>
2026-04-28 16:33:33 -0400

    __init__.py

--- geochem/__init__.py
+++ geochem/__init__.py
@@ -4,9 +4,9 @@ geochem
 Porewater geochemistry and headspace gas depth profile figures.
 
 Modules:
-  geochem_figures -- load_iw, load_gc, plot_C0019J, plot_C0019M, plot_combined
+  geochem_analysis -- load_iw, load_gc, plot_C0019J, plot_C0019M, plot_combined
 """
-from geochem.geochem_figures import (
+from geochem.geochem_analysis import (
     load_iw,
     load_gc,
     plot_C0019J,
