b2e67e999487d7d1dd3f69f461910b21ae26e7a6
Rene Castillo <110206995+rocknrene@users.noreply.github.com>
2026-04-28 16:24:04 -0400

    __init__.py

--- core_ml/__init__.py
+++ core_ml/__init__.py
@@ -1,1 +1,11 @@
-from .labeler import VCDLabeler
+"""
+core_ml
+=======
+Visual Core Description labeling and JCORES mining tools.
+
+Modules:
+  labeler -- JCORESMiner (automated PDF extraction) and VCDLabeler (interactive)
+"""
+from core_ml.labeler import JCORESMiner, VCDLabeler, STABILITY_MAP, JCORES_LEXICON
+
+
