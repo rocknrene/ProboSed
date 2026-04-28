fe9caad780d179a7a20cc06cb486c0a0e0290a71
Rene Castillo <110206995+rocknrene@users.noreply.github.com>
2026-04-28 18:25:21 -0400

    __init__.py

--- transport/__init__.py
+++ transport/__init__.py
@@ -10,8 +10,4 @@ model in slope/stability.py. All computations use pure NumPy.
 Modules:
   agents -- SedimentAgentModel, calculate_clast_distribution, forcing_from_slope
 """
-from transport.agents import (
-    SedimentAgentModel,
-    calculate_clast_distribution,
-    forcing_from_slope,
-)
+
